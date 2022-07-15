fda330e7f5933074c890558e9d5840b899a6bf11
Jakub <kuba.jur03@gmail.com>
2022-07-15 15:35:13 +0200

    added RWD

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,9 +6,14 @@ const computerChoiceBtn = document.querySelector(".computer-choice-img");
 const resultBtn = document.querySelector(".result-btn");
 const resultText = document.querySelector(".text-result");
 const resetBtn = document.querySelector(".reset-btn");
+const clearBtn = document.querySelector('.clear-btn');
+const playerPointsField = document.querySelector(".player-points");
+const computerPointsField = document.querySelector(".computer-points");
 
 let playerChoice = null;
 let computerChoice = null;
+let playerPoints = 0;
+let computerPoints = 0;
 const computerChoices = ["rock", "paper", "scissors"];
 
 const whoWin = (playerChoice, computerChoice) => {
@@ -24,6 +29,8 @@ const whoWin = (playerChoice, computerChoice) => {
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
+    playerPoints += 1;
+    playerPointsField.textContent = playerPoints;
     return "You won!";
   }
   if (
@@ -31,6 +38,8 @@ const whoWin = (playerChoice, computerChoice) => {
     (playerChoice === "rock" && computerChoice === "paper") ||
     (playerChoice === "paper" && computerChoice === "scissors")
   ) {
+    computerPoints += 1;
+    computerPointsField.textContent = computerPoints;
     return "You lost!";
   }
 };
@@ -38,34 +47,13 @@ const whoWin = (playerChoice, computerChoice) => {
 let afterCheckingResult = false;
 
 const selectHandler = (choice) => {
-  if (afterCheckingResult) {
-    return;
-  }
-//   playerChoiceBtn.src = `../assets/images/${choice}.png`;
+  if (afterCheckingResult) return;
   playerChoiceBtn.src = `./assets/images/${choice}.png`;
-  playerChoiceBtn.style.height = "130px";
   playerChoice = choice;
   resultBtn.disabled = false;
 };
 
-rockBtn.addEventListener("click", selectHandler.bind(null, "rock"));
-paperBtn.addEventListener("click", selectHandler.bind(null, "paper"));
-scissorsBtn.addEventListener("click", selectHandler.bind(null, "scissors"));
-
-resultBtn.addEventListener("click", () => {
-  if (!playerChoice) {
-    return;
-  }
-  afterCheckingResult = true;
-  computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  computerChoiceBtn.src = `./assets/images/${computerChoice}.png`;
-  const result = whoWin(playerChoice, computerChoice);
-  resultText.textContent = result;
-  resetBtn.disabled = false;
-  resultBtn.disabled = true;
-});
-
-resetBtn.addEventListener("click", () => {
+const resetGame = () => {
   playerChoice = null;
   computerChoice = null;
   resultText.textContent = null;
@@ -74,4 +62,39 @@ resetBtn.addEventListener("click", () => {
   afterCheckingResult = false;
   playerChoiceBtn.src = `./assets/images/question.png`;
   computerChoiceBtn.src = `./assets/images/question.png`;
+  computerChoiceBtn.style.transform = "scaleX(1)";
+};
+
+rockBtn.addEventListener("click", selectHandler.bind(null, "rock"));
+paperBtn.addEventListener("click", selectHandler.bind(null, "paper"));
+scissorsBtn.addEventListener("click", selectHandler.bind(null, "scissors"));
+
+resultBtn.addEventListener("click", () => {
+  if (!playerChoice) return;
+
+  if (playerPoints > 0 || computerPoints > 0) {
+    clearBtn.disabled = false;
+  }
+  
+  afterCheckingResult = true;
+  computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  computerChoiceBtn.src = `./assets/images/${computerChoice}.png`;
+  computerChoiceBtn.style.transform = "scaleX(-1)";
+  const result = whoWin(playerChoice, computerChoice);
+  resultText.textContent = result;
+  resetBtn.disabled = false;
+  resultBtn.disabled = true;
+});
+
+resetBtn.addEventListener("click", () => {
+ resetGame();
+});
+
+clearBtn.addEventListener('click', () => {
+  playerPoints = 0;
+  computerPoints = 0;
+  playerPointsField.textContent = 0;
+  computerPointsField.textContent = 0;
+  clearBtn.disabled = true;
+  resetGame();
 });
